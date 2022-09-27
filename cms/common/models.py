ccd623a388506c1562d0d2029b4938cebd80f182
Patrick Farrell <mr.patfarrell@gmail.com>
2022-09-27 11:11:18 -0400

    updated post model to allow ordering by date property

--- cms/common/models.py
+++ cms/common/models.py
@@ -6,9 +6,14 @@ from datetime import date
 
 class MarkdownPost(frontmatter.Post):
     def __init__(self, kebab, title, content, id=None, dt=None):
-        super().__init__(content=content, kebab=kebab, title=title, date=dt, published=False)
+        super().__init__(
+            content=content, kebab=kebab, title=title, date=dt, published=False
+        )
 
         if self.get("id") is None:
             self.metadata["id"] = int(time.time())
         if self.get("date") is None:
             self.metadata["date"] = str(date.today())
+
+    def __lt__(self, other):
+        return self["date"] < other["date"]
